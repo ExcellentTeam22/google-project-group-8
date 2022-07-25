fb7ef7c59c82e4470e9107383335e86f786715ba
Eliachar Feig <efeig15@gmail.com>
2022-07-25 11:24:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from dataclasses import dataclass
+from typing import List
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@dataclass
+class AutoCompleteData:
+    completed_sentence: str
+    source_text: str
+    offset: int
+    score: int
+# methods that you need to define by yourself
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_best_k_completions(prefix: str) -> List[AutoCompleteData]:
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
