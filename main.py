50c3918f12f903daa5059c98f7e9e44c94962830
Eliachar Feig <efeig15@gmail.com>
2022-07-25 11:42:36 +0300

    Update

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 from typing import List
 
+from autocomplete import AutoCompleter
+
+import sqlite3       
+
 @dataclass
 class AutoCompleteData:
     completed_sentence: str
@@ -10,7 +14,21 @@ class AutoCompleteData:
 # methods that you need to define by yourself
 
 
+def build_database():
+    pass
+
+
+def initialization(path):
+    f = open(path, 'r', encoding='utf-8')
+    all_lines = f.readlines()
+    f.close()
+    return all_lines
+
+
 def get_best_k_completions(prefix: str) -> List[AutoCompleteData]:
     pass
 
 
+if __name__=='__main__':
+
+    pass
